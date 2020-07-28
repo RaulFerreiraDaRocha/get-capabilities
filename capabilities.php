a245f8c9a87afa273cdb37ce823b3df2841e98de
RaulFerreiraDaRocha <raulferreiradarocha@hotmail.com>
2020-07-28 16:44:20 -0300

    vendo capacidades de usuario
    apenas para guardar o código

---
+++ capabilities.php
@@ -0,0 +1,11 @@
+<?php
+
+$data = get_userdata( get_current_user_id() );
+ 
+if ( is_object( $data) ) {
+    $current_user_caps = $data->allcaps;
+     
+    // print it to the screen
+    echo '<pre>' . print_r( $current_user_caps, true ) . '</pre>';
+}
+?>
